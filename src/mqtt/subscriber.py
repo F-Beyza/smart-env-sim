3fc7075e7dbbaf71959da4eefe7be2706bc04753
F-Beyza <fatmabeyzaaydogan@gmail.com>
2025-08-07 16:51:52 +0300

    config

--- src/mqtt/subscriber.py
+++ src/mqtt/subscriber.py
@@ -1,35 +1,6 @@
 # src/mqtt/subscriber.py
 
-<<<<<<< HEAD
- #       import paho.mqtt.client as mqtt
-  #      import json
-   #     from mongo_handler.mongo_handler import MongoHandler
-#
- #       mongo_handler = MongoHandler()
-#
- #       def on_connect(client, userdata, flags, rc):
-  #          print("[✓] Connected to MQTT broker")
- #           client.subscribe("sensor/data")
-#
-  #      def on_message(client, userdata, msg):
-   #         payload = msg.payload.decode()
-    #        print("[•] Message received:", payload)
-#
-     #       try:
- #               data = json.loads(payload)
-  #              mongo_handler.insert_data(data)
-   #             print("[✓] Data inserted into MongoDB")
-    #        except json.JSONDecodeError:
-     #           print("[✗] Invalid JSON format")
-#
- #       client = mqtt.Client()
-  #      client.on_connect = on_connect
-   #     client.on_message = on_message
-#
- #       client.connect("localhost", 1883, 60)
-  #      print("[•] Waiting for messages...")
-   #     client.loop_forever()
-=======
+
 import paho.mqtt.client as mqtt
 import json
 
@@ -41,14 +12,6 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 
 from mongo_handler.mongo_handler import MongoHandler
->>>>>>> refactor-mqtt-callbacks
-
-
-
-# src/mqtt/subscriber.py
-
-
-
 from mqtt.mqtt_client import MQTTClient
 
 
